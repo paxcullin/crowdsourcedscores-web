9c801570dfa1081071fbf67fd5b6e31ceb1dcf1c
Chris Aronchick <chrisaronchick@gmail.com>
2018-08-14 07:40:31 -0700

    More updates to profile

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -4,6 +4,23 @@
 // Unauthenticated getGroup Details
 
 
+function addUserProfileAttributes(userProfileDetails2) {
+    var attributeNames = [];
+    for (var i=0;i < userProfileDetails2.length; i++) {
+        attributeNames.push(userProfileDetails2[i].Name)
+    }
+
+    console.log("userProfileDetails2[attributeNames.indexOf(\"preferred_username\")]: ", userProfileDetails2[attributeNames.indexOf("preferred_username")])
+    if (attributeNames.indexOf("preferred_username")) {
+        $("#preferred_username").val(userProfileDetails2[attributeNames.indexOf("preferred_username")].Value);
+    } else {
+        $("#preferred_username").text(userProfileDetails["cognito:username"]);
+    }
+    $("#name").text(userProfileDetails2[attributeNames.indexOf("name")].Value);
+    $("#profileEmail").val(userProfileDetails2[attributeNames.indexOf("email")].Value);
+    return userProfileDetails;
+}
+
 var userProfileInfo = function () {
     var cognitoUser = userPool.getCurrentUser();
     cognitoUser.getSession(function (err, session) {
@@ -18,36 +35,43 @@ var userProfileInfo = function () {
         // for (var d in userProfileDetails) {
         //     $("#profileDetails").append("<strong>" + d + "</strong>: " + userProfileDetails[d] + "<br>")
         // }
-        $("#name").text(userProfileDetails.given_name + " " + userProfileDetails.family_name);
-        $("#profileEmail").val(userProfileDetails.email);
-        return userProfileDetails;
+        var userProfileDetails2 = cognitoUser.getUserAttributes(function(err, result){
+            if (result) { addUserProfileAttributes(result);}
+        });
+        // console.log("userProfileDetails: ", userProfileDetails)
+
     });
 };
 
 function updateProfile () {
-    console.log("update button clicked")
-    var attributeList = [];
-    var attribute = {
-        Name : 'preferred_username',
-        Value : $("#preferred_username").val()
-    };
-    var attribute = new AmazonCognitoIdentity.CognitoUserAttribute(attribute);
-    attributeList.push(attribute);
-
     
-    console.log("attributeList: ", attributeList);
-    cognitoUser.updateAttributes(attributeList, function(err, result) {
+    //console.log("useToken cognitoUser = ", cognitoUser);
+    cognitoUser.getSession(function (err, session) {
         if (err) {
-            alert(err);
-            return;
+            //window.location = '/';
+            console.log("getSession err = ", JSON.stringify(err))
         }
-        console.log('call result: ' + result);
+        console.log("session = ", session)
+        console.log("update button clicked")
+        var attributeList = [];
+        var attribute = {
+            Name : 'preferred_username',
+            Value : $("#preferred_username").val()
+        };
+        var attribute = new AmazonCognitoIdentity.CognitoUserAttribute(attribute);
+        attributeList.push(attribute);
+    
+        
+        console.log("attributeList: ", attributeList);
+        cognitoUser.updateAttributes(attributeList, function(err, result) {
+            if (err) {
+                console.log(JSON.stringify(err));
+                if (err.code === "AliasExistsException") {
+                    $("#updateErrorText").text("Someone already has that username. Please select another one.")
+                }
+                return;
+            }
+            console.log('call result: ' + result);
+        });
     });
 };
-
-
-
-// for (i = 0; i < result.length; i++) {
-//     $("#profileDetails").append(result[i])
-//     console.log('attribute ' + result[i].getName() + ' has value ' + result[i].getValue());
-// }
