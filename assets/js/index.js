4af95fc85149334e0190a0d892f6fae3ab06efa9
Chris Aronchick <chrisaronchick@gmail.com>
2018-08-24 16:17:38 -0700

    Updated styling for results view

--- assets/js/index.js
+++ assets/js/index.js
@@ -109,3 +109,19 @@ function createCrowd() {
         })
     })
 }
+
+$( "#predictionScoreDialog" ).dialog({
+    autoOpen: false,
+    show: {
+      effect: "blind",
+      duration: 1000
+    },
+    hide: {
+      effect: "explode",
+      duration: 1000
+    }
+  });
+
+$('.predictionScore').on('click', function() {
+    $("#predictionScoreDialog").dialog("open");
+})
