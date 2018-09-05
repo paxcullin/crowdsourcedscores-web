5790698ce6ca2eeb47bd2d602f8cfca7ce95db5a
Chris Aronchick <chrisaronchick@gmail.com>
2018-09-05 14:44:29 -0700

    Added collapse functionality - replaced moving boxes.

--- assets/js/index.js
+++ assets/js/index.js
@@ -199,3 +199,25 @@ $("#dropdown").on("click", function(e){
       $(this).children("ul").slideUp("fast");
     }
   });
+
+  $(".toggle-accordion").on("click", function() {
+    var accordionId = $(this).attr("accordion-id"),
+      numPanelOpen = $(accordionId + ' .collapse.in').length;
+    
+    $(this).toggleClass("active");
+
+    if (numPanelOpen == 0) {
+      openAllPanels(accordionId);
+    } else {
+      closeAllPanels(accordionId);
+    }
+  })
+
+  openAllPanels = function(aId) {
+    console.log("setAllPanelOpen");
+    $(aId + ' .panel-collapse:not(".in")').collapse('show');
+  }
+  closeAllPanels = function(aId) {
+    console.log("setAllPanelclose");
+    $(aId + ' .panel-collapse.in').collapse('hide');
+  }
