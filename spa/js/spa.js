264185b3a8059a8dedfcc9bc3b008eecc465bf6d
Chris Aronchick <chrisaronchick@gmail.com>
2018-10-05 18:06:01 -0700

    Through chapter 3.4.4

--- spa/js/spa.js
+++ spa/js/spa.js
@@ -13,11 +13,7 @@
 
 var spa = (function () {
     var initModule = function ( $container ) {
-        $container.html(
-            '<h1 style="display:inline-block; margin: 25px;">'
-            + 'Hello World!'
-            + '</h1>'
-        );
+        spa.shell.initModule ($container);
     };
     return { initModule: initModule };
 })();
