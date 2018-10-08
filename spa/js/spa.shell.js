57b44d2b03d9aa2c9c17c2e80937b6e7ea5cd3ca
Chris Aronchick <chrisaronchick@gmail.com>
2018-10-08 09:10:13 -0700

    Chapter 3.6 update

--- spa/js/spa.shell.js
+++ spa/js/spa.shell.js
@@ -120,7 +120,11 @@ spa.shell = (function() {
 
     //-------- BEGIN EVENT HANDLERS ---------//
     onClickChat = function (event) {
-      toggleChat(stateMap.is_chat_retracted);
+      if ( toggleChat(stateMap.is_chat_retracted) ) {
+        $.uriAnchor.setAnchor({
+          chat: ( stateMap.is_chat_retracted ? 'open' : 'closed' )
+        });
+      }
       return false;
     };
 
