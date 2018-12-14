ad7d4213a55a98e4f45c801139d10ae01fec0d49
Chris Aronchick (COMPUCOM SYSTEMS INC) <v-chraro@microsoft.com>
2018-12-14 11:44:55 -0800

    Stripe Lambda function setup

---
+++ lambda/stripePayment/index.js
@@ -0,0 +1,14 @@
+// Set your secret key: remember to change this to your live secret key in production
+// See your keys here: https://dashboard.stripe.com/account/apikeys
+var stripe = require("stripe")("sk_test_BY4ytB8ozxXglTzsejJAh0tO");
+
+// Token is created using Checkout or Elements!
+// Get the payment token ID submitted by the form:
+const token = request.body.stripeToken; // Using Express
+
+const charge = stripe.charges.create({
+  amount: 999,
+  currency: 'usd',
+  description: 'Example charge',
+  source: token,
+});
