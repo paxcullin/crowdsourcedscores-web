4bad6518d09af2963ff1e524c9804d688211c0e8
Chris Aronchick <chrisaronchick@gmail.com>
2020-07-18 16:06:12 -0700

    Updated EPL odds lambda

--- lambda/dataScraper-python-epl/lambda_function.py
+++ lambda/dataScraper-python-epl/lambda_function.py
@@ -1,7 +1,10 @@
 #import urllib.request
-import json, urllib3
+import json, urllib3, boto3
 from datetime import datetime
 http = urllib3.PoolManager()
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('games-soccer-epl')
+
 urls = ['https://io.oddsshark.com/scores/epl/2020-06-27']
 
 def lambda_handler(event, context):
@@ -10,7 +13,9 @@ def lambda_handler(event, context):
         data = json.loads(request.data)
         
         for item in data:
-            gameId = item['event_id']
+            item['gameId'] = item['event_id']
+            gameId = item['gameId']
+            table.put_item(Item=item)
             print(gameId)
         return data
             # print(gameId)
