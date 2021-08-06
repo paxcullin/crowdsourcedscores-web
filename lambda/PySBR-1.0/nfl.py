fa5563218e879335d19322bbb14882bb32e047be
Chris Aronchick <chrisaronchick@gmail.com>
2021-08-05 23:49:44 -0700

    points and spreads are working

--- lambda/PySBR-1.0/nfl.py
+++ lambda/PySBR-1.0/nfl.py
@@ -1,8 +1,9 @@
-import collections
+import pandas as pd
 from pysbr import *
 from pysbr.config.config import Config
 from datetime import datetime
 from pymongo import MongoClient
+
 
 
 
@@ -17,11 +18,19 @@ cols = ['event', 'event id', 'participant', 'spread / total', 'decimal odds', 'a
 nfl = NFL()
 sb = Sportsbook()
 e = EventsByDateRange(nfl.league_id, startDate,endDate)
-cl = CurrentLines(e.ids(), nfl.market_ids('pointspread'), sb.ids('Pinnacle')[0])
-print(len(e.list()))
+spreads = CurrentLines(e.ids(), nfl.market_ids('pointspread'), sb.ids('Pinnacle')[0])
+totals = CurrentLines(e.ids(), nfl.market_ids('totals'), sb.ids('Pinnacle')[0])
+# lines = pd.merge(spreads.dataframe(), totals.dataframe(), how="outer", on="event id")
+
+print('totals', len(totals.list()))
+
+
+# print(cl.list()[0])
 if len(e.list()) > 0:
-    for event in e.list()[0:4]:
-        print(event)
+    for event in e.list():
+        homeId = ''
+        # try:
+            # print(event)
         gameObject = {
                 "gameId": event['event id'],
                 "season": "reg",
@@ -31,25 +40,58 @@ if len(e.list()) > 0:
                 "status": event['event status'],
                 "sport": "nfl",
                 "location": event['location'] + ", " + event["country"],
-                "startDateTime": datetime.strptime(event["datetime"], '%Y-%m-%dT%H:%M:%S%z') 
+                "odds": {
+                    "spread": '',
+                    "total": ''
+                },
+                "startDateTime": datetime.strptime(event["datetime"], '%Y-%m-%dT%H:%M:%S%z')
             }
         for team in event['participants']:
             teamObject = {
+                    "participantId": team["participant id"],
                     "code": team["source"]["abbreviation"],
                     "shortName": team["source"]["nickname"],
                     "fullName": team["source"]["name"] + " " + team["source"]["nickname"]
                 }
             if team['is home'] == True:
                 gameObject["homeTeam"] = teamObject
+                homeId = team['participant id']
             else:
                 gameObject["awayTeam"] = teamObject
-        collection.update_one({
-            "gameId": event['event id']
-            },
-            {
-                "$set": gameObject
-            },
-            upsert=True)
+        if len(spreads.list()) > 0:
+            # print(homeId)
+            for spread in spreads.list():
+                # print(spread['event id'] == gameObject['gameId'], spread['participant id'] == gameObject["homeTeam"]["participantId"])
+                if (spread['event id'] == gameObject['gameId'] and spread['participant id'] == gameObject["homeTeam"]["participantId"]):
+                    # print(spread)
+                    gameObject['odds']['spread'] = spread['spread / total']
+
+            # if line['event id'] == event['event id']:
+            #     print(line, event['event id'])
+        # print(len(totals.list()))
+        if len(totals.list()) > 0:
+            # print(homeId)
+            for total in totals.list():
+                print(total)
+                if (total['event id'] == gameObject['gameId']):
+                    print(total['spread / total'], gameObject['homeTeam']['code'])
+                    gameObject['odds']['total'] = total['spread / total']
+
+            # if line['event id'] == event['event id']:
+            #     print(line, event['event id'])
+        if gameObject["gameWeek"] == 1:
+            # print(gameObject)
+            collection.update_one({
+                "gameId": event['event id']
+                },
+                {
+                    "$set": gameObject
+                },
+                upsert=True)
+        # except TypeError:
+        #     print(TypeError, event)
+        # except ValueError:
+        #     print(ValueError)
 
 # print(cl.dataframe(e)[cols])
 # for event in e.list():
