6dbd69b3fef7ae1a207b85faed4117f6f781b8f2
Chris Aronchick <chrisaronchick@gmail.com>
2023-12-13 10:03:45 -0800

    Extended profile is adding currency by default

--- lambda/user-getExtendedProfile/index.js
+++ lambda/user-getExtendedProfile/index.js
@@ -8,16 +8,12 @@ var mongo = require("mongodb").MongoClient,
 
 // console.log('Loading function');
 
-exports.handler = (event, context) => {
+exports.handler = async (event, context) => {
     console.log('Received event:', JSON.stringify(event, null, 2));
-
-    mongo.connect(MONGO_URL, function (err, client) {
-        assert.equal(null, err);
-        if(err) {
-            context.done(err, null);
-        }
-
-        const db = client.db('pcsm');
+    try {
+    const client = await mongo.connect(MONGO_URL);
+    const db = client.db('pcsm');
+    const collection = db.collection('profileExtended');
         
         let projectObj = { "username": event.username }
         event.sport ? projectObj.sport = event.sport : null
@@ -25,15 +21,20 @@ exports.handler = (event, context) => {
         event.season ? projectObj.season = event.season : null
         event.week ? projectObj.week = event.week : null
 
-        var collection = db.collection('profileExtended');
-        collection.findOne({"username":event.username}, {username: 1, groups: { $elemMatch: { sport: projectObj.sport, year: projectObj.year } }, results: 1})
+    const extendedProfile = await collection.findOne({"username":event.username}, {username: 1, groups: { $elemMatch: { sport: projectObj.sport, year: projectObj.year } }, results: 1})
         //, [`results['${projectObj.sport}'][${projectObj.year}]['${projectObj.season}'].overall`]: 1, [`results['${projectObj.sport}'][${projectObj.year}]['${projectObj.season}'].weekly`]: { $elemMatch: { gameWeek: event.week }}
-        .then(function(extendedProfile) {
-            assert.equal(err, null);
-            if(err) {
-                context.done(err, null);
+        if (!extendedProfile) {
+            context.done(null, {status: 200, message: "No extended profile found"});
+        }
+        console.log('extendedProfile', extendedProfile.currency);
+        if (extendedProfile && (extendedProfile.currency === null || extendedProfile.currency === undefined))  {
+            let updatedProfile = await collection.updateOne({username: event.username}, {$set: {currency: 10000}})
+            console.log('updatedProfile', updatedProfile)
+            if (updatedProfile.modifiedCount === 1) {
+                extendedProfile.currency = 10000
             }
-            console.log("extendedProfile: ", JSON.stringify(extendedProfile))
+        }
+            // console.log("extendedProfile: ", JSON.stringify(extendedProfile))
             if (event.sport && event.year && event.season && event.week && extendedProfile.results && extendedProfile.results[event.sport] && extendedProfile.results[event.sport][event.year] && extendedProfile.results[event.sport][event.year][event.season]) {
                 let overallResults = extendedProfile.results[event.sport][event.year][event.season].overall
                 let weeklyResultFilteredArray = extendedProfile.results[event.sport][event.year][event.season].weekly.filter((weeklyResult) => {
@@ -53,7 +54,8 @@ exports.handler = (event, context) => {
                 }
             }
             context.done(null, extendedProfile);
-        })
-        .catch(function(reject) {console.log("reject: ", reject)});
-    });
+    } catch (err) {
+        console.log(err);
+        context.done(err, null);
+    }
 };
