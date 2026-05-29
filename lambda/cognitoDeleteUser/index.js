822fc9dfae91ed1e365a8b855c5804d1a4cbd4d1
Chris Aronchick <chrisaronchick@gmail.com>
2026-05-28 17:36:57 -0700

    Fixed cognitoDeleteUser to use latest

--- lambda/cognitoDeleteUser/index.js
+++ lambda/cognitoDeleteUser/index.js
@@ -1,9 +1,12 @@
-const aws = require('aws-sdk'),
-        CognitoIdentityServiceProvider = aws.CognitoIdentityServiceProvider,
-        client = new CognitoIdentityServiceProvider({ apiVersion: '2016-04-19', region: 'us-west-2' }),
-        mongo = require("mongodb").MongoClient,
+const  mongo = require("mongodb").MongoClient,
         assert = require("assert");
-const MONGO_URL = 'mongodb://${username}:${password}@ds011775.mlab.com:11775/pcsm';
+
+const { CognitoIdentityProviderClient, AdminDeleteUserCommand } = require("@aws-sdk/client-cognito-identity-provider");
+const cognitoClient = new CognitoIdentityProviderClient({ region: 'us-west-2' });
+const {config} = require('./config');
+
+const MONGO_URL = `mongodb+srv://${config.username}:${config.password}@pcsm.lwx4u.mongodb.net/pcsm?retryWrites=true&w=majority`;
+
 
 //var provider = new client.AWSCognitoIdentityProviderClient();
 //provider.adminDeleteUser(req); 
@@ -15,32 +18,29 @@ var cognitoData = {
 
 //now you can call adminDeleteUser on the client object    
 
-exports.handler = (event, context, callback) => {
+exports.handler = async (event, context, callback) => {
+    console.log('event :>> ', event);
     // TODO implement
-    client.adminDeleteUser({Username: event.username, UserPoolId: 'us-west-2_zym3aCbQ3'}, function(err, data) {
-      if (err) {
-          console.log(err, err.stack); // an error occurred
-          context.done(err, null);
-      } else {
-          console.log(data);           // successful response
-          mongo.connect(MONGO_URL, (err, dbResponse) => {
-              if (err) {
-                context.done(null, "success");
-              }
-              let db = dbResponse.db('pcsm');
-              let coll = db.collection('profileExtended');
-              coll.findOneAndDelete({ username: event.username })
-              .then(deleteResponse => {
-                  console.log({deleteResponse});
-                  context.done(null, "success");
-              })
-              .catch(deleteError => {
-                  console.log({deleteError});
-                  context.done(null, 'error')
-              })
-          })
-      }
-    })
+    try {
+        const deleteCognitoResponse = await cognitoClient.send(new AdminDeleteUserCommand({Username: event.username, UserPoolId: 'us-west-2_zym3aCbQ3'}));
+            console.log(deleteCognitoResponse);           // successful response
+            const dbResponse = await mongo.connect(MONGO_URL);
+                let db = dbResponse.db('pcsm');
+                let coll = db.collection('profileExtended');
+            const deleteResponse = await coll.findOneAndDelete({ username: event.username });
+                console.log({deleteResponse});
+                return {
+                    status: 200,
+                    message: JSON.stringify({ success: "User deleted successfully", deleteResponse })
+                };
+            
+    } catch(deleteUserError) {
+        console.log({deleteUserError});
+        return {
+            status: 500,
+            message: JSON.stringify({ error: "Failed to delete user", details: deleteUserError })
+        }
+    }
     
 //     client.adminUpdateUserAttributes({
 //   "UserAttributes": [ 
